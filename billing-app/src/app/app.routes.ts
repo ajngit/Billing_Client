cdfc6ae80f52d2f8e5e4d2329ba73dffdb442f62
abhinavgit2002 <abhinavjnair61@gmail.com>
2025-02-20 21:27:44 +0530

    Design

--- billing-app/src/app/app.routes.ts
+++ billing-app/src/app/app.routes.ts
@@ -1,3 +1,28 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './Components/home/home.component';
+import { BillingComponent } from './Components/billing/billing.component';
+import { AddProductComponent } from './Components/add-product/add-product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'login', component:LoginComponent
+    },
+    {
+        path:'',component:HomeComponent //,canActivate: [AuthGuard]
+      },
+      {
+        path:'billing',component:BillingComponent
+      },
+      {
+        path:'addproduct',component:AddProductComponent
+      },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+export class AppRoutingModule { }
+  
