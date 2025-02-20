4bcdfb67b4ddb4c6be69435a7ea8512c801e249f
abhinavgit2002 <abhinavjnair61@gmail.com>
2025-02-20 21:45:48 +0530

    env

---
+++ billing-app/src/appsettings.ts
@@ -0,0 +1,9 @@
+export const environment = {
+
+    // production: true, 
+    // apiUrl: 'https://billingserver.onrender.com' 
+
+    production: false, 
+    apiUrl: 'http://localhost:3001'
+  };
+  
